8665913b4dc3b21fb8a572279177be724bb9411c
sagarverma2602 <sagar189202115@gmail.com>
2022-09-27 09:22:38 +0000

    db created

--- backend/tables/mydb.js
+++ backend/tables/mydb.js
@@ -6,11 +6,12 @@ var con = mysql.createConnection({
   password: "team8@1234"
 });
 
-con.connect(function(errr) {
-  if (errr) throw errr;
+con.connect(function(err) {
+  if (err) throw err;
   console.log("Connected!");
-  con.query("CREATE DATABASE mydb", function (errr, result) {
-    if (errr) throw errr;
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
     console.log("Database created");
   });
 });
+// create table expenseModel(expenseId varchar(256),billNumber int,billImage varbinary(max),billCost int,datedOn date,status varchar(256),remark varchar(256),claimedBy varchar(256),Primary key (expenseId),foreign key(claimedBy) references userModel(email))
