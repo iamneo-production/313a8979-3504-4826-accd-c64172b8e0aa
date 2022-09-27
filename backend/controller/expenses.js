e9a7013da56b9d483ddf7831561607d5839678c2
piyushpp07 <piyushpar7@gmail.com>
2022-09-27 10:27:34 +0000

    expense backend things

--- backend/controller/expenses.js
+++ backend/controller/expenses.js
@@ -18,3 +18,34 @@ const createExpense = (req, res, next) => {
     //format query , [data] , callback
  }
  
+ const deleteExpense = (req, res, next) => {
+   let expense = req.body
+   const query =
+      "delete from expenseModel where expenseId = ?"
+   connection.query(query, 
+       [expense.expenseId],
+      (err, results) => {
+         if (!err) {
+            return res.status(200).json({ message: "Expense Delete Successfully" })
+         }
+         else {
+            return res.status(500).json({ message: "Expense adding failed" })
+         }
+      });
+   //format query , [data] , callback
+}
+const updateExpense = (req, res, next) =>{
+   let expense = req.body
+   const query =
+      "update expenseModel set billCost = ? where expenseId = ?"
+   connection.query(query, 
+       [expense.billCost,expense.expenseId],
+      (err, results) => {
+         if (!err) {
+            return res.status(200).json({ message: "Expense Updated Successfully" })
+         }
+         else {
+            return res.status(500).json({ message: "Expense adding failed" })
+         }
+      });  
+}
