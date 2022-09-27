94bac862c6e40242c5eee2b6f011da3cd54ce31c
harry536 <singhharendra255@gmail.com>
2022-09-27 16:04:46 +0000

    frontend login and signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,44 +5,52 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Login from './components/Login'
 import Signup from './components/Signup'
+import Home from './components/Home'
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              Tarrif Manager
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
 
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-up" element={<Signup />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
+    <Home/>
+    // <Router>
+    //   <div className="App">
+    //     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+    //       <div className="container">
+    //         <Link className="navbar-brand" to={'/sign-in'}>
+    //           Tarrif Manager
+    //         </Link>
+    //         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    //           <ul className="navbar-nav ml-auto">
+    //           <li className="nav-item">
+    //               <Link className="nav-link" to={'/sign-in'}>
+    //                 Home
+    //               </Link>
+    //             </li>
+    //             <li className="nav-item">
+    //               <Link className="nav-link" to={'/sign-in'}>
+    //                 Login
+    //               </Link>
+    //             </li>
+    //             <li className="nav-item">
+    //               <Link className="nav-link" to={'/sign-up'}>
+    //                 Sign up
+    //               </Link>
+    //             </li>
+    //           </ul>
+    //         </div>
+    //       </div>
+    //     </nav>
+
+    //     <div className="auth-wrapper">
+    //       <div className="auth-inner">
+    //         <Routes>
+    //           <Route exact path="/" element={<Login />} />
+    //           <Route path="/sign-in" element={<Login />} />
+    //           <Route path="/sign-up" element={<Signup />} />
+    //         </Routes>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </Router>
   )
 }
 
