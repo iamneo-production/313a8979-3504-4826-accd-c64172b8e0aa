6156372bc2d3a328376229d47fdb2d186c62e4fb
piyushpp07 <piyushpar7@gmail.com>
2022-09-27 17:49:16 +0000

    signup linked

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const authRouter = require('./routes/auth')
 const app = express()
 
 app.use(express.json());
- 
+app.use(cors())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,47 +10,47 @@ import Home from './components/Home'
 function App() {
   return (
 
-    <Home/>
-    // <Router>
-    //   <div className="App">
-    //     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-    //       <div className="container">
-    //         <Link className="navbar-brand" to={'/sign-in'}>
-    //           Tarrif Manager
-    //         </Link>
-    //         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-    //           <ul className="navbar-nav ml-auto">
-    //           <li className="nav-item">
-    //               <Link className="nav-link" to={'/sign-in'}>
-    //                 Home
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to={'/sign-in'}>
-    //                 Login
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link className="nav-link" to={'/sign-up'}>
-    //                 Sign up
-    //               </Link>
-    //             </li>
-    //           </ul>
-    //         </div>
-    //       </div>
-    //     </nav>
 
-    //     <div className="auth-wrapper">
-    //       <div className="auth-inner">
-    //         <Routes>
-    //           <Route exact path="/" element={<Login />} />
-    //           <Route path="/sign-in" element={<Login />} />
-    //           <Route path="/sign-up" element={<Signup />} />
-    //         </Routes>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Router>
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              Tarrif Manager
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<Signup />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   )
 }
 
