8dc0bb9568038274f644c5fa2c1c0ee554e8e81d
piyushpp07 <piyushpar7@gmail.com>
2022-09-28 15:07:41 +0000

    dfsd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ const App = () => {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <Link to={"/"} className="navbar-brand">
-          Tarriff
+          Tarriff Manager
         </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
